81c30cab2b194f8c2fa918c7dc4b6247cfda942f
WillChang <r1000chang@gmail.com>
2017-07-12 00:12:19 +0800

    debug

--- WillBot/Controllers/BDController.cs
+++ WillBot/Controllers/BDController.cs
@@ -13,26 +13,26 @@ namespace WillBot.Controllers
     public class BDController : ApiController
     {
         [HttpPost]
-        public IHttpActionResult POST(string rawdata)
+        public IHttpActionResult POST()
         {
             try
             {
                 BaseService bs = new BaseService();
                 StockService ss = new StockService();
                 //取得LINE POST過來的JSON資料
-                //var rawdata = Request.Content.ReadAsStringAsync().Result;
+                var rawdata = Request.Content.ReadAsStringAsync().Result;
                 //序列化成物件
-                LineMessageApiSDK.LineReceivedObject.LineReceivedMsg ReceivedObject = JsonConvert.DeserializeObject<LineMessageApiSDK.LineReceivedObject.LineReceivedMsg>(rawdata);
+                //LineMessageApiSDK.LineReceivedObject.LineReceivedMsg ReceivedObject = JsonConvert.DeserializeObject<LineMessageApiSDK.LineReceivedObject.LineReceivedMsg>(rawdata);
                 //取得event物件
-                var eventObj = ReceivedObject.events[0];
-                string message = string.Empty;
+                //var eventObj = ReceivedObject.events[0];
+                //string message = string.Empty;
 
-                if (bs.IsCallMe(eventObj.message.text, out message))
-                {
-                    message = ss.GetOneStock(message);
-                }
+                //if (bs.IsCallMe(eventObj.message.text, out message))
+                //{
+                //    message = ss.GetOneStock(message);
+                //}
                  
-                return Ok(message);
+                return Ok(rawdata);
             }
             catch (Exception ex)
             {
